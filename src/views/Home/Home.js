1684531294a6cdf5df689b8b4539ae56696e64c4
clouddawn <1993456181@qq.com>
2022-12-01 21:39:15 +0800

    健康码

---
+++ src/views/Home/Home.js
@@ -0,0 +1,76 @@
+import qs from "qs";
+import {Dialog, Toast} from "vant";
+import dayjs from "dayjs";
+
+export default {
+  data() {
+    return {
+      nowTime:"",
+      hesuanTime:""
+    };
+  },
+  mounted() {
+    setInterval(()=>{
+      this.nowTime = dayjs().format("MM-DD HH:mm:ss");
+      this.hesuanTime = dayjs().add(-1,'day').format("MM月DD日")
+    },200)
+  },
+  methods: {
+    signIn() {
+      let url = new URLSearchParams(window.location.search);
+      let result = url.get("code");
+      if (this.$route.query.code || result) {
+        let params;
+        params = qs.stringify({
+          code: this.$route.query.code || result,
+          certNo: this.$route.query.certNo,
+          certName: this.$route.query.certName,
+          mobile: this.$route.query.mobile,
+        });
+        Toast.loading({
+          message: "加载中...",
+          forbidClick: true,
+        });
+        this.$axios
+          .post("/userLogin", params)
+          .then((res) => {
+            Toast.clear();
+            if (res.data.result !== 1) {
+              Dialog.alert({
+                message: res.data.reason,
+              }).then(() => {
+              });
+            }
+          })
+          .catch((err) => {
+            Toast.clear();
+            console.error(err);
+            Dialog.alert({
+              message: "接口调用失败",
+            }).then(() => {
+            });
+          });
+      } else {
+        Toast.loading({
+          message: "加载中...",
+          forbidClick: true,
+        });
+        this.$axios.post("/isLogin")
+          .then((res) => {
+            Toast.clear();
+            if (res.data.result !== 1) {
+              Dialog.alert({
+                message: res.data.reason,
+              }).then(() => {
+              });
+            }
+          })
+          .catch((err) => {
+            Toast.clear();
+            console.log(err);
+            Toast("调用接口失败");
+          });
+      }
+    },
+  }
+};
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <img class="health_code" src="@/assets/healthCode.jpg" alt="">
+    <div class="health_code_text">苏康码·绿码</div>
+    <div class="name">陈二光</div>
+    <div class="number">
+      <div class="phoneNumber test">132****8691</div>
+      <div class="idNumber test">342******559</div>
+    </div>
+    <div class="now_time">{{ nowTime }}</div>
+    <div class="collect_date">{{hesuanTime}}核酸检测</div>
+    <div class="result_date">{{hesuanTime}}核酸检测</div>
+  </main>
+</template>
+
+<script src="./Home.js"></script>
+
+<style lang="scss" scoped>
+@import "./Home.scss";
+</style>
