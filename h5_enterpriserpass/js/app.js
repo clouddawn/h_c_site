bc1b0582501c575adce2932c5e7d9d4475289394
clouddawn <1993456181@qq.com>
2022-12-03 18:12:53 +0800

    修改publicPathâ

--- h5_enterpriserpass/js/app.js
+++ h5_enterpriserpass/js/app.js
@@ -203,7 +203,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/h_c_site/h5_enterpriserpass/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
